27650f6fab4b4181cf77f1347841b8ab11e9367e
Michael Liu <mfliu@cs.ubc.ca>
2023-05-23 18:00:39 -0700

    Refactor CelebAHQ to read data from cache directory

--- ldm/data/celebahq.py
+++ ldm/data/celebahq.py
@@ -1,49 +1,32 @@
-from torch.utils.data import Dataset
+import os
 from PIL import Image
 from torchvision.datasets import ImageFolder
-from torchvision.transforms import Resize, CenterCrop, ToTensor, Compose
 import numpy as np
-
-# datapath = "/home/saiyuel/Downloads/celebahq256_imgs/train"
-
-# CelebAHQTrain = ImageFolder(
-#     root="/home/saiyuel/Downloads/celebahq256_imgs/train",
-#     transform=Compose([Resize(config.size), CenterCrop(config.size), ToTensor()]
-#                       )
-#
-# CelebAHQTest = ImageFolder(
-#     root="/home/saiyuel/Downloads/celebahq256_imgs/valid",
-#     transform=Compose([Resize(config.size), CenterCrop(config.size), ToTensor()]
-#                       )
 
 
 class CelebAHQBase(ImageFolder):
-    def __init__(self, root, size, transform=None, **kwargs):
+    def __init__(self, datadir, size, transform=None, **kwargs):
         # self.root = root
         self.size = size
         self.transform = transform
-        super().__init__(
-            root=root,
-            **kwargs)
+        cachedir = os.environ.get("XDG_CACHE_HOME", os.path.expanduser("~/.cache"))
+        super().__init__(root=os.path.join(cachedir, datadir), **kwargs)
 
     def __getitem__(self, index):
         image, _ = super().__getitem__(index)
-
         img = np.array(image).astype(np.uint8)
 
         crop = min(img.shape[0], img.shape[1])
         h, w, = img.shape[0], img.shape[1]
-        img = img[(h - crop) // 2:(h + crop) // 2,
-              (w - crop) // 2:(w + crop) // 2]
+        img = img[
+            (h - crop) // 2:(h + crop) // 2,
+            (w - crop) // 2:(w + crop) // 2
+        ]
 
-        image = Image.fromarray(img)  ## convert array to Image
+        image = Image.fromarray(img)
         if self.size is not None:
             image = image.resize((self.size, self.size))
-
         image = np.array(image).astype(np.uint8)
-
-        # print('final size is : ', image.shape) final size is :  (32, 32, 3)
-        breakpoint()
 
         example = {"image": (image / 127.5 - 1.0).astype(np.float32)}
 
@@ -52,12 +35,9 @@ class CelebAHQBase(ImageFolder):
 
 class CelebAHQTrain(CelebAHQBase):
     def __init__(self, **kwargs):
-        super().__init__(root="/home/saiyuel/Downloads/celebahq256_imgs/train", **kwargs)
+        super().__init__(datadir="CelebAHQ", **kwargs)
 
 
 class CelebAHQTest(CelebAHQBase):
     def __init__(self, **kwargs):
-        super().__init__(root="/home/saiyuel/Downloads/celebahq256_imgs/valid", **kwargs)
-
-
-
+        super().__init__(datadir="CelebAHQ", **kwargs)
